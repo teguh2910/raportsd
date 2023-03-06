29a6b553f8c694562813edc136bc5a1b06a91317
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-03-06 20:34:14 +0700

    change_db

---
+++ db_raport.sql
@@ -0,0 +1,271 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 06, 2023 at 08:32 PM
+-- Server version: 8.0.32-0ubuntu0.22.04.2
+-- PHP Version: 8.1.2-1ubuntu2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_raport`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_guru`
+--
+
+CREATE TABLE `data_guru` (
+  `id_guru` int NOT NULL,
+  `nama_guru` varchar(40) NOT NULL,
+  `jabatan` varchar(10) NOT NULL,
+  `id_user` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data_guru`
+--
+
+INSERT INTO `data_guru` (`id_guru`, `nama_guru`, `jabatan`, `id_user`) VALUES
+(2, 'sri', 'wali_kelas', NULL),
+(3, 'teguh', 'wali_kelas', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `id_siswa` int NOT NULL,
+  `nama_siswa` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `alamat` text NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `id_user` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data_siswa`
+--
+
+INSERT INTO `data_siswa` (`id_siswa`, `nama_siswa`, `alamat`, `kelas`, `id_user`) VALUES
+(2, 'teguh', 'solo', '4', NULL),
+(4, 'rizky', 'Karawang', '3', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasus_siswa`
+--
+
+CREATE TABLE `kasus_siswa` (
+  `id_kasus` int NOT NULL,
+  `id_siswa` int NOT NULL,
+  `kasus` text NOT NULL,
+  `tgl_kasus` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kasus_siswa`
+--
+
+INSERT INTO `kasus_siswa` (`id_kasus`, `id_siswa`, `kasus`, `tgl_kasus`) VALUES
+(2, 2, 'Tawuran', '2023-03-05'),
+(3, 4, 'Gelut', '2023-03-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mata_pelajaran`
+--
+
+CREATE TABLE `mata_pelajaran` (
+  `id_pelajaran` int NOT NULL,
+  `nama_mata_pelajaran` varchar(30) NOT NULL,
+  `id_guru` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `mata_pelajaran`
+--
+
+INSERT INTO `mata_pelajaran` (`id_pelajaran`, `nama_mata_pelajaran`, `id_guru`) VALUES
+(2, 'Matematika', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai_siswa`
+--
+
+CREATE TABLE `nilai_siswa` (
+  `id_nilai` int NOT NULL,
+  `id_siswa` int NOT NULL,
+  `id_pelajaran` int NOT NULL,
+  `nilai` int NOT NULL,
+  `semester` int NOT NULL,
+  `tahun` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `nilai_siswa`
+--
+
+INSERT INTO `nilai_siswa` (`id_nilai`, `id_siswa`, `id_pelajaran`, `nilai`, `semester`, `tahun`) VALUES
+(2, 2, 2, 100, 2, 2022),
+(3, 4, 2, 100, 2, 2022),
+(4, 2, 2, 100, 3, 2022),
+(5, 2, 2, 100, 1, 2022);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presensi_siswa`
+--
+
+CREATE TABLE `presensi_siswa` (
+  `id_presensi` int NOT NULL,
+  `id_siswa` int NOT NULL,
+  `id_pelajaran` int NOT NULL,
+  `presensi` varchar(10) NOT NULL,
+  `tgl` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `presensi_siswa`
+--
+
+INSERT INTO `presensi_siswa` (`id_presensi`, `id_siswa`, `id_pelajaran`, `presensi`, `tgl`) VALUES
+(2, 2, 2, 'Hadir', '2023-03-05'),
+(3, 4, 2, 'Hadir', '2023-03-06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `hak_akses` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `hak_akses`) VALUES
+(1, 'siswa', 'siswa', 'siswa'),
+(2, 'guru', 'guru', 'guru'),
+(3, 'admin', 'admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_guru`
+--
+ALTER TABLE `data_guru`
+  ADD PRIMARY KEY (`id_guru`);
+
+--
+-- Indexes for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- Indexes for table `kasus_siswa`
+--
+ALTER TABLE `kasus_siswa`
+  ADD PRIMARY KEY (`id_kasus`);
+
+--
+-- Indexes for table `mata_pelajaran`
+--
+ALTER TABLE `mata_pelajaran`
+  ADD PRIMARY KEY (`id_pelajaran`);
+
+--
+-- Indexes for table `nilai_siswa`
+--
+ALTER TABLE `nilai_siswa`
+  ADD PRIMARY KEY (`id_nilai`);
+
+--
+-- Indexes for table `presensi_siswa`
+--
+ALTER TABLE `presensi_siswa`
+  ADD PRIMARY KEY (`id_presensi`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_guru`
+--
+ALTER TABLE `data_guru`
+  MODIFY `id_guru` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `data_siswa`
+--
+ALTER TABLE `data_siswa`
+  MODIFY `id_siswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `kasus_siswa`
+--
+ALTER TABLE `kasus_siswa`
+  MODIFY `id_kasus` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `mata_pelajaran`
+--
+ALTER TABLE `mata_pelajaran`
+  MODIFY `id_pelajaran` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `nilai_siswa`
+--
+ALTER TABLE `nilai_siswa`
+  MODIFY `id_nilai` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `presensi_siswa`
+--
+ALTER TABLE `presensi_siswa`
+  MODIFY `id_presensi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
