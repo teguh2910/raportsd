0208d9dea226b07f39c85b7fd71978fcb5db8dec
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-03-11 09:44:40 +0700

    wotr

--- config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("localhost:3306","vufweeyc_testaja","@Qwerty24","localhost:3306	");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
