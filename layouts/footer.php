5dd61fbe5804a312b556114e64702ec1a41cbf1f
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-03-11 08:05:06 +0700

    finalr0

--- layouts/footer.php
+++ layouts/footer.php
@@ -2,10 +2,10 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      E-Raport
+      PenilaianSiswaSD
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2022 <a href="#">E-Raport</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="#">PenilaianSiswaSD</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
