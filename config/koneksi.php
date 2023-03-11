538a725759864af74c621d621812be57842b920f
TEGUH YUHONO <98807221+teguh2910@users.noreply.github.com>
2023-03-11 08:13:23 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","db_raport");
+$koneksi = mysqli_connect("localhost:3306","vufweeyc_raportsd","@Qwerty24","vufweeyc_testaja");
  
 // Check connection
 if (mysqli_connect_errno()){
