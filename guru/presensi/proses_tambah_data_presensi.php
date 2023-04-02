c64c5e8b2028f97e1d7c816585c5b7808502be82
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-04-02 22:36:51 +0700

    sds

--- guru/presensi/proses_tambah_data_presensi.php
+++ guru/presensi/proses_tambah_data_presensi.php
@@ -6,6 +6,13 @@ include '../../config/koneksi.php';
 $id_guru       = $_POST['id'];
 $tgl           = $_POST['tanggal'];
 
+$query="SELECT * FROM presensi_siswa WHERE tgl='$tgl'";
+$result = mysqli_query($koneksi, $query);
+$cek = mysqli_num_rows($result);
+if($cek > 0){
+  echo "<script>alert('Data sudah ada.');window.location='../data_presensi.php';</script>";
+}else{
+
 $query1="SELECT * FROM data_siswa INNER JOIN data_guru ON data_siswa.kelas = data_guru.kelas WHERE data_guru.id_guru='$id_guru'";
 $result1 = mysqli_query($koneksi, $query1);
 while($data = mysqli_fetch_assoc($result1)){
@@ -15,3 +22,4 @@ while($data = mysqli_fetch_assoc($result1)){
   $result = mysqli_query($koneksi, $query);
 }
   echo "<script>alert('Data berhasil ditambah.');window.location='../data_presensi.php';</script>";
+}
